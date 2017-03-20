c5c192d0bb58acc4c9b51f1a73590e67d43817ff
jorye <jinyu.wang@heysroad.com>
2017-03-20 15:07:31 +0800

    创建工程

---
+++ YKiOSCategory/YKiOSCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YKiOSCategory
+//
+//  Created by zte on 2017/3/20.
+//  Copyright © 2017年 youkiC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YKiOSCategory/YKiOSCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YKiOSCategory
+//
+//  Created by zte on 2017/3/20.
+//  Copyright © 2017年 youkiC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
